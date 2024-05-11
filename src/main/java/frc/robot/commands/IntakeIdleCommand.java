37785dcb81bdafee98bbff6090fd7d2e160f22bd
14380 <95910962+14380@users.noreply.github.com>
2024-05-11 21:12:03 +1000

    First Add

---
+++ src/main/java/frc/robot/commands/IntakeIdleCommand.java
@@ -0,0 +1,21 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.IntakeSubsystem.WantedState;
+
+public class IntakeIdleCommand extends Command
+{
+    private final IntakeSubsystem intakeSubSystem;
+
+    public IntakeIdleCommand(IntakeSubsystem subsystem) {
+        intakeSubSystem = subsystem;
+        // Use addRequirements() here to declare subsystem dependencies.
+        addRequirements(subsystem);
+      }
+
+      @Override
+  public void initialize() {
+    intakeSubSystem.setWantedState(WantedState.IDLE);
+  }
+}
