014ab7e5eeaa813cbbb23a613febac389fdec923
Daniel P <daniel@learnweaver.com>
2024-06-19 08:38:22 +1000

    Rumble

--- src/main/java/frc/robot/commands/ControllerRumbleCommand.java
+++ src/main/java/frc/robot/commands/ControllerRumbleCommand.java
@@ -9,10 +9,19 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 public class ControllerRumbleCommand extends Command {
     private CommandXboxController controller;
     private BooleanSupplier condition;
+    
+    private int loopCtr;
+    private int m_time;
 
     public ControllerRumbleCommand(CommandXboxController controller, BooleanSupplier condition) {
         this.controller = controller;
         this.condition = condition;
+        m_time = (int) (0.5 * 50);// 20ms in 1 sec
+    }
+
+    @Override
+    public void initialize() {
+        loopCtr = 0;
     }
 
     @Override
@@ -25,5 +34,10 @@ public class ControllerRumbleCommand extends Command {
     @Override
     public void end(boolean interrupted) {
         controller.getHID().setRumble(GenericHID.RumbleType.kBothRumble, 0.0);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return loopCtr > m_time;
     }
 }
